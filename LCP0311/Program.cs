65ebe3ad5a90d9e91ef582ffd435902ae02e46b3
Alexandra Camarena <alexandracamarena@ou.edu>
2024-03-10 20:04:48 -0500

    Add project files.

---
+++ LCP0311/Program.cs
@@ -0,0 +1,33 @@
+﻿// MIS 3013 001
+// March 11, 2024
+// Alexandra Camarena
+// 113568154
+
+Console.WriteLine("Exam 2 Practice 1");
+
+string menuStr = "*************************";
+
+menuStr = $"{menuStr}\nSales System Menu";
+menuStr = $"{menuStr}\n1. Add a new order";
+menuStr = $"{menuStr}\n2. Show summary information";
+menuStr = $"{menuStr}\n*************************";
+
+while (true)
+{ 
+    Console.WriteLine(menuStr);
+
+    Console.Write("Enter an option: ");
+    string userChoiceStr= Console.ReadLine();
+
+    if (userChoiceStr == "1")
+    { // add a new order
+    }
+    else if (userChoiceStr == "2")
+    { // show summary information
+    }
+    else
+    {
+        Console.WriteLine("Thank you and goodbye!");
+        break;
+    }
+}
